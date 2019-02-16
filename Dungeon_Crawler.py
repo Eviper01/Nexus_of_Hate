81b62ead2ae8e97455fe8d51d72ef4af94b3c204
John Wallis <eviper01@gmail.com>
2019-02-16 23:01:25 +0800

    Commit 2
    ~

--- Dungeon_Crawler.py
+++ Dungeon_Crawler.py
@@ -1,28 +1,60 @@
 import math
 import random
+# debug
+
+
+debug_weapon = {
+    "Name" : "Worldsmasher",
+    "VitMod" : 10.0,
+    "EndMod" : 20.0,
+    "IntMod" : 5.0,
+    "MinVit" : 0.0,
+    "MinEnd" : 0.0,
+    "MinInt" : 0.0,
+    "Type"  : "Weapon",
+    }
+# skill arrays represent stat modify *Vit + *End + *Int + Flat
+# possibilities of speicals calling methods
+# negative costs for healing
+#Type Passive or Active
+debug_attack = {
+    "Name": "Celestial Swipe",
+    "Type": "Acitve",
+    "HpCost": ["0","0","0","0"],
+    "SpCost": ["0","0","0","0"],
+    "MpCost": ["0","0","0","0"],
+    "single": ["1","1","1","50"],
+    "all": ["0","0","0","50"],
+
+}
+
 
 # GAMEDATA
 
 # multipliers for the stas of each type - Knight[0], Ranger[1], Mage[2]
 types = [[1.5,1.0,1.0],[1.0,1.5,1.0],[1.0,1.0,1.5]]
-enemy_Sprites = [["<","(","x","_","x",")","<"] , ["<","(","O","^","O",")",">"],["<","(","V","_","V",")",">","*"]]
+#these could be beter
+enemy_Sprites = [["<","(","x","_","x",")","<"] , ["<","(","O","^","O",")",">"],["<","(","V","_","V",")",">","*"]]\
+# these could be better
+enemy_Names = ["Abrielle","Adair","Adara","Adriel","Aiyana","Alissa","Alixandra","Altair","Amara","Anatola","Anya","Arcadia","Ariadne","Arianwen","Aurelia","Aurelian","Aurelius","Avalon","Acalia","Alaire","Auristela","Bastian","Breena","Brielle","Briallan","Briseis","Cambria","Cara","Carys","Caspian","Cassia","Cassiel","Cassiopeia","Cassius","Chaniel","Cora","Corbin","Cyprian","Daire","Darius","Destin","Drake","Drystan","Dagen","Devlin","Devlyn","Eira","Eirian","Elysia","Eoin","Evadne","Eliron","Evanth","Fineas","Finian","Fyodor","Gareth","Gavriel","Griffin","Guinevere","Gaerwn","Ginerva","Hadriel","Hannelore","Hermione","Hesperos","Iagan","Ianthe","Ignacia","Ignatius","Iseult","Isolde","Jessalyn","Kara","Kerensa","Korbin","Kyler","Kyra","Katriel","Kyrielle","Leala","Leila","Lilith","Liora","Lucien","Lyra","Leira","Liriene","Liron","Maia","Marius","Mathieu","Mireille","Mireya","Maylea","Meira","Natania","Nerys","Nuriel","Nyssa","Neirin","Nyfain","Oisin","Oralie","Orion","Orpheus","Ozara","Oleisa","Orinthea","Peregrine","Persephone","Perseus","Petronela","Phelan","Pryderi","Pyralia","Pyralis","Qadira","Quintessa","Quinevere","Raisa","Remus","Rhyan","Rhydderch","Riona","Renfrew","Saoirse","Sarai","Sebastian","Seraphim","Seraphina","Sirius","Sorcha","Saira","Sarielle","Serian","Séverin","Tavish","Tearlach","Terra","Thalia","Thaniel","Theia","Torian","Torin","Tressa","Tristana","Uriela","Urien","Ulyssia","Vanora","Vespera","Vasilis","Xanthus","Xara","Xylia","Yadira","Yseult","Yakira","Yeira","Yeriel","Yestin","Zaira","Zephyr","Zora","Zorion","Zaniel","Zarek"]
 
-
-#   <(x_x)<
-#
-#
-#
 #BACKEND
 def new_enemy(Pos,type,level=1):
     stats = {
-    "Name" : 
+    "Name" : Get_EnemyName(),
     "HP" : math.sqrt(level)*1000.0,
     "Damage" : math.sqrt(level)*25.0,
     "Type" : type,
-    "Pos" : Pos
+    "Pos" : Pos,
     }
     return stats
 
+def Get_EnemyName():
+    n1 = random.choice(enemy_Names)
+    n2 = random.choice(enemy_Names)
+    while n1 == n2:
+        n2 = random.choice(enemy_Names)
+    return(n1 + " " + n2)
 
 def level_player(type,level=1):
     stats = {
@@ -32,10 +64,11 @@ def level_player(type,level=1):
     "level" : level,
     "item 1" : "empty",
     "item 2" : "empty",
-    "weapon" : "none",
-    "armour" : "none",
-    "ring" : "none",
-
+    "weapon" : {"Name": "none"},
+    "armour" : {"Name": "none"},
+    "ring" : {"Name": "none"},
+    "inv" : [],
+    "skills":[],
     }
     stats["MaxHp"] = math.sqrt(stats["Vitality"]) * 1000.0
     stats["MaxSp"] = math.sqrt(stats["Endurance"]) * 1000.0
@@ -56,12 +89,31 @@ def new_item(health_gain = 0, stamina_gain = 0, mana_gain = 0, keyLevel = 0):
 def Game_Tick():
     for enemy in enemy_list:
         player["CurrentHp"] = player["CurrentHp"] - enemy["Damage"]
-        print( enemy["HP"])
+        print(enemy["Name"] + ": " + str(enemy["HP"]))
     print("Your HP: " + str(player["CurrentHp"]) + "/" + str(player["MaxHp"]))
     print("Your SP: " + str(player["CurrentSp"]) + "/" + str(player["MaxSp"]))
     print("Your MP: " + str(player["CurrentMp"]) + "/" + str(player["MaxMp"]))
+    if player["CurrentHp"] <= 0:
+        Lose()
+def Lose():
+    #calculate score here
+    score = 0
+    print("You Lost, Final Score: " + str(score))
 
-
+def show_Inventory(player_id):
+    print("Weapon:" + str(player_id["weapon"]))
+    print("Armour:" + str(player_id["armour"]))
+    print("Ring:" + str(player_id["ring"]))
+    print("Carrying:")
+    for item in player_id["inv"]:
+        print(item)
+    q = input("Equip?:")
+    if q in player_id["inv"]:
+        pass
+    else:
+        print("unable to equip")
+def show_Skills(player_id):
+    pass
 #MEMORY DATA
 
 #FRONTEND
@@ -96,17 +148,22 @@ def Make_Room(enemies, l=100, h=30):
 def Get_input():
     command = input(">")
     if command == "inv":
-        pass
-    elif command == "up":
-        pass
-    elif command == "down":
-        pass
-    elif command == "left":
-        pass
-    elif command == "right":
+        if True:
+            show_Inventory(player)
+            Get_input()
+        else:
+            print("unable to acess inventory, there are enemies nearby")
+    elif command == "skills":
+        if True:
+            show_Skills(player)
+            Get_input()
+        else:
+            print("unable to check skills, there are enemies nearby")
+    elif command == "continue" and True:
         pass
     elif command == "help":
-        pass
+        print("attack 1-3,\nitem 1-2,\ncontinue,\nsave,\ninv or skills")
+        Get_input()
     elif command == "attack 1":
         pass
     elif command == "attack 2":
@@ -123,16 +180,17 @@ def Get_input():
         print("invalid command")
         Get_input()
 
+#Init
 enemy_list = []
 enemy_list.append(new_enemy((25,25),0))
 enemy_list.append(new_enemy((6,13),0))
 enemy_list.append(new_enemy((12,9),1))
 enemy_list.append(new_enemy((19,55),2))
-
-
 player = level_player(1)
+player["weapon"] = debug_weapon
 print(player)
 
+# Game loop
 while True:
     Make_Room(enemy_list)
     Get_input()
