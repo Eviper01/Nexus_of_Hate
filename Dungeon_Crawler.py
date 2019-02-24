7b60a72eb954a70aec74f56c56818d80bf708b7e
John Wallis <eviper01@gmail.com>
2019-02-24 12:58:39 +0800

    Save/Load and Item Procedural Generation
    To Do
    Item data
    Item Modifiers
    Leveling System
    Map/Movement
    Better Enemy Sprites and 2D rendering
    
    Also:
    Ensure Code is ready to run in a sever client Configuration

--- Dungeon_Crawler.py
+++ Dungeon_Crawler.py
@@ -1,15 +1,19 @@
 import math
 import random
+from ast import literal_eval
 
 # GAMEDATA
 
 #Skills - have name and a method attached to them
 
+def none(a=0,b=0,c=0):
+    pass
+
 def Celestial_Slash(player_id):
     fail = True
     while fail == True:
         try:
-            enemy = enemy_list[int(input("Target #?"))]
+            enemy = enemy_list[int(input("Target #?"))-1]
             enemy["CurrentHp"] = enemy["CurrentHp"] - (player_id["Weapon"]["VitMod"]*player_id["Vitality"] + player_id["Weapon"]["EndMod"]*player_id["Endurance"] + player_id["Weapon"]["IntMod"]*player_id["Intelligence"] + 50)
             fail = False
         except:
@@ -18,7 +22,7 @@ def Celestial_Slash(player_id):
     for enemy in enemy_list:
         enemy["CurrentHp"] = enemy["CurrentHp"] - 50
 
-
+# Modifiers
 
 
 # multipliers for the stas of each type - Knight[0], Ranger[1], Mage[2]
@@ -28,6 +32,75 @@ enemy_Sprites = [["<","(","x","_","x",")","<"] , ["<","(","O","^","O",")",">"],[
 # these could be better
 enemy_Names = ["Abrielle","Adair","Adara","Adriel","Aiyana","Alissa","Alixandra","Altair","Amara","Anatola","Anya","Arcadia","Ariadne","Arianwen","Aurelia","Aurelian","Aurelius","Avalon","Acalia","Alaire","Auristela","Bastian","Breena","Brielle","Briallan","Briseis","Cambria","Cara","Carys","Caspian","Cassia","Cassiel","Cassiopeia","Cassius","Chaniel","Cora","Corbin","Cyprian","Daire","Darius","Destin","Drake","Drystan","Dagen","Devlin","Devlyn","Eira","Eirian","Elysia","Eoin","Evadne","Eliron","Evanth","Fineas","Finian","Fyodor","Gareth","Gavriel","Griffin","Guinevere","Gaerwn","Ginerva","Hadriel","Hannelore","Hermione","Hesperos","Iagan","Ianthe","Ignacia","Ignatius","Iseult","Isolde","Jessalyn","Kara","Kerensa","Korbin","Kyler","Kyra","Katriel","Kyrielle","Leala","Leila","Lilith","Liora","Lucien","Lyra","Leira","Liriene","Liron","Maia","Marius","Mathieu","Mireille","Mireya","Maylea","Meira","Natania","Nerys","Nuriel","Nyssa","Neirin","Nyfain","Oisin","Oralie","Orion","Orpheus","Ozara","Oleisa","Orinthea","Peregrine","Persephone","Perseus","Petronela","Phelan","Pryderi","Pyralia","Pyralis","Qadira","Quintessa","Quinevere","Raisa","Remus","Rhyan","Rhydderch","Riona","Renfrew","Saoirse","Sarai","Sebastian","Seraphim","Seraphina","Sirius","Sorcha","Saira","Sarielle","Serian","Séverin","Tavish","Tearlach","Terra","Thalia","Thaniel","Theia","Torian","Torin","Tressa","Tristana","Uriela","Urien","Ulyssia","Vanora","Vespera","Vasilis","Xanthus","Xara","Xylia","Yadira","Yseult","Yakira","Yeira","Yeriel","Yestin","Zaira","Zephyr","Zora","Zorion","Zaniel","Zarek"]
 
+loot_levels = ["Poor ","Common ","Unique ","Legendary ","Mythical ","Fabled ",]
+loot_levels_potions = ["Weak ","Lesser ","Standard ","Concentrated ","Greater ","Supreme ",]
+
+
+Weapons = [
+ {
+    "Name" : "Shortsword",
+    "VitMod" : 5.0,
+    "EndMod" : 5.0,
+    "IntMod" : 0.0,
+    "MinVit" : 0.0,
+    "MinEnd" : 0.0,
+    "MinInt" : 0.0,
+    "Weight" : "Light",
+    "Type"  : "Weapon",
+    },
+]
+Weapon_Modifiers = [none]
+Armours = [
+{
+    "Name" : "Chainmail",
+    "DEF" : 10.0,
+    "VitGain" : 0.0,
+    "EndGain" : 0.0,
+    "IntGain" : 0.0,
+    "MinVit" : 0.0,
+    "MinEnd" : 0.0,
+    "MinInt" : 0.0,
+    "Weight" : "Heavy",
+    "Type"  : "Armour",
+},
+]
+Armour_Modifiers = [none]
+Rings = [{"Name": "none"}]
+Ring_Modifiers = [none]
+Potions = [
+{
+"Name" : "Healing Potion",
+"HpGain" : 10,
+"MpGain": 0,
+"SpGain": 0,
+"Type": "item",
+"Subtype": "Potion",
+},
+{
+"Name" : "Stamina Potion",
+"HpGain" : 0,
+"MpGain": 10,
+"SpGain": 0,
+"Type": "item",
+"Subtype": "Potion",
+},
+{
+"Name" : "Mana Potion",
+"HpGain" : 0,
+"MpGain": 10,
+"SpGain": 0,
+"Type": "item",
+"Subtype": "Potion",
+},
+{
+"Name" : "Energy Potion",
+"HpGain" : 0,
+"MpGain": 5,
+"SpGain": 5,
+"Type": "item",
+"Subtype": "Potion",
+},
+]
 #BACKEND
 
 def find(lst, key, value):
@@ -35,6 +108,77 @@ def find(lst, key, value):
         if dic[key] == value:
             return i
     return -1
+
+def Get_Loot_Level(x):
+    chance = random.random()
+    level = (math.ceil(math.log(x,4)))
+    if chance <= 0.3:
+         level = level - 1
+    elif chance >= 0.7:
+        level = level + 1
+    else:
+        level = level
+    if level <= 0:
+        return 1
+    else:
+        return level
+
+
+def Points_to_Percent(x):
+    return 100-(1000/math.sqrt((2*x)+100))
+
+
+def Generate_Potion(loot_level):
+    item = random.choice(Potions).copy()
+    item["HpGain"] = item["HpGain"]*loot_level
+    item["SpGain"] = item["SpGain"]*loot_level
+    item["MpGain"] = item["MpGain"]*loot_level
+    item["Rarity"] = loot_level
+    item["Name"] = loot_levels_potions[loot_level-1] + item["Name"]
+    item["id"] = random.random()
+    return item
+
+def Generate_Weapon(loot_level):
+    item = random.choice(Weapons).copy()
+    Modifier = none
+    if random.random() < 0.2:
+        Modifier = random.choice(Weapon_Modifiers)
+    item["VitMod"]  = item["VitMod"]*(loot_level)
+    item["EndMod"]  = item["EndMod"]*(loot_level)
+    item["IntMod"]  = item["IntMod"]*(loot_level)
+    item["MinVit"]  = item["MinVit"]*(loot_level)
+    item["MinEnd"]  = item["MinEnd"]*(loot_level)
+    item["MinInt"]  = item["MinInt"]*(loot_level)
+    item["Rarity"] = loot_level
+    item["Modifier"] = Modifier
+    item["Name"] = loot_levels[loot_level-1] + item["Name"]
+    return item
+
+def Generate_Armour(loot_level):
+    item = random.choice(Armours).copy()
+    Modifier = none
+    if random.random() < 0.2:
+        Modifier = random.choice(Armour_Modifiers)
+    item["DEF"] = item["DEF"]*(loot_level)
+    item["MinVit"]  = item["MinVit"]*(loot_level)
+    item["MinEnd"]  = item["MinEnd"]*(loot_level)
+    item["MinInt"]  = item["MinInt"]*(loot_level)
+    item["VitGain"]  = item["VitGain"]*(loot_level)
+    item["EndGain"]  = item["EndGain"]*(loot_level)
+    item["IntGain"]  = item["IntGain"]*(loot_level)
+    item["Rarity"] = loot_level
+    item["Modifier"] = Modifier
+    item["Name"] = loot_levels[loot_level-1] + item["Name"]
+    return item
+
+def Generate_Ring():
+    item = random.choice(Rings).copy()
+    Modifier = none
+    if random.random() < 0.2:
+        Modifier = random.choice(Ring_Modifiers)
+    item["Modifier"] = Modifier
+    return item
+
 
 def Generate_Room():
     global Combat
@@ -50,6 +194,7 @@ def new_enemy(Pos,type,level=1):
     "Damage" : math.sqrt(level)*random.randint(20,30),
     "Type" : type,
     "Pos" : Pos,
+    "Level": level,
     }
     return stats
 
@@ -59,17 +204,7 @@ def Get_EnemyName():
     while n1 == n2:
         n2 = random.choice(enemy_Names)
     return(n1 + " " + n2)
-#stats
-#
-# Vitality, Endurance, Intelligence
-# Defense, Dodge, Crit
-# HP, SP, MP
-#
-#
-#
-#
-#
-#
+
 
 def new_player(type,level=1):
     stats = {
@@ -78,8 +213,8 @@ def new_player(type,level=1):
     "Endurance" : level*types[type][1],
     "Intelligence" : level*types[type][2],
     "level" : level,
-    "item 1" : {"Name": "none"},
-    "item 2" : {"Name": "none"},
+    "item 1" : {"Name": "none", "Subtype": "none"},
+    "item 2" : {"Name": "none", "Subtype": "none"},
     "Attack 1": {"Name":"none", "Method":none},
     "Attack 2": {"Name":"none", "Method":none},
     "Attack 3": {"Name":"none", "Method":none},
@@ -99,8 +234,7 @@ def new_player(type,level=1):
     stats["CurrentMp"] = stats["MaxMp"]
     return stats
 
-def none(player_id):
-    Get_input()
+
 
 def level_player(player_id):
     print("level up!")
@@ -119,7 +253,17 @@ def level_player(player_id):
 
 
 def Damage(attacker,target):
-    target["CurrentHp"] = target["CurrentHp"] - attacker["Damage"]
+    if target["Armour"] == {"Name": "none"}:
+        target["CurrentHp"] = target["CurrentHp"] - attacker["Damage"]
+    else:
+        if target["Armour"]["Weight"] == "Heavy":
+            target["CurrentHp"] = target["CurrentHp"] - (attacker["Damage"]*(100-Points_to_Percent(target["Armour"]["DEF"]))/100)
+        if target["Armour"]["Weight"] == "Light":
+            chance = random.random()
+            if chance <= target["Armour"]["DEF"]:
+                pass
+            else:
+                target["CurrentHp"] = target["CurrentHp"] - attacker["Damage"]
 
 def Game_Tick():
     for enemy in enemy_list:
@@ -147,6 +291,20 @@ def Room_Cleared():
     player["CurrentMp"] = player["MaxMp"]
     # Grant an item
     print("Room Cleared! ,Check inv for new items")
+    chance = random.random()
+    if chance <= 0.8:
+        player["inv"].append(Generate_Potion(Get_Loot_Level(player["level"])))
+    else:
+        if chance <= 0.5:
+            player["inv"].append(Generate_Ring())
+        elif chance <= 0.8:
+            player["inv"].append(Generate_Armour(Get_Loot_Level(player["level"])))
+        else:
+            player["inv"].append(Generate_Weapon(Get_Loot_Level(player["level"])))
+
+
+
+
 
 def Lose():
     #calculate score here
@@ -156,22 +314,51 @@ def Lose():
     Lost = True
 
 def show_Inventory(player_id):
+    player_id["inv"] = [i for n, i in enumerate(player_id["inv"]) if i not in player_id["inv"][n + 1:]]
     print("Weapon:" + str(player_id["Weapon"]))
     print("Armour:" + str(player_id["Armour"]))
     print("Ring:" + str(player_id["Ring"]))
+    print("Item 1:" + str(player_id["item 1"]))
+    print("Item 2:" + str(player_id["item 2"]))
     print("Carrying:")
     for item in player_id["inv"]:
         print(item)
     q = input("Equip?:")
     if any(d['Name'] == q for d in player_id["inv"]):
         item =  player_id["inv"][find(player_id["inv"],"Name",q)]
-        temp = player_id[item["Type"]].copy()
-        player_id[item["Type"]] = item.copy()
-        player_id["inv"][find(player_id["inv"],"Name",q)] = temp
+        if item["Type"] == "item":
+            slot = input("Which Slot?")
+            if slot == "1":
+                temp = player_id["item 1"].copy()
+                player_id["item 1"] = item.copy()
+                player_id["inv"][find(player_id["inv"],"Name",q)] = temp
+            elif slot =="2":
+                temp = player_id["item 2"].copy()
+                player_id["item 2"] = item.copy()
+                player_id["inv"][find(player_id["inv"],"Name",q)] = temp
+            else:
+                print("unable to equip")
+        else:
+            temp = player_id[item["Type"]].copy()
+            player_id[item["Type"]] = item.copy()
+            player_id["inv"][find(player_id["inv"],"Name",q)] = temp
     else:
         print("unable to equip")
 
+def Use_Potion(slot,player_id):
+    player_id["CurrentHp"] = player_id["CurrentHp"]+(player_id["MaxHp"]*player_id[slot]["HpGain"]/100)
+    player_id["CurrentSp"] = player_id["CurrentSp"]+(player_id["MaxSp"]*player_id[slot]["MpGain"]/100)
+    player_id["CurrentMp"] = player_id["CurrentMp"]+(player_id["MaxMp"]*player_id[slot]["SpGain"]/100)
+    if player_id["CurrentHp"] > player_id["MaxHp"]:
+        player_id["CurrentHp"] = player_id["MaxHp"]
+    if player_id["CurrentSp"] > player_id["MaxSp"]:
+        player_id["CurrentSp"] = player_id["MaxSp"]
+    if player_id["CurrentMp"] > player_id["MaxMp"]:
+        player_id["CurrentMp"] = player_id["MaxMp"]
+    player_id[slot] = {"Name": "none", "Subtype": "none"}
+
 def show_Skills(player_id):
+    player_id["skills"] = [i for n, i in enumerate(player_id["skills"]) if i not in player_id["skills"][n + 1:]]
     print("Attack 1:" + str(player_id["Attack 1"]))
     print("Attack 2:" + str(player_id["Attack 2"]))
     print("Attack 3:" + str(player_id["Attack 3"]))
@@ -279,48 +466,96 @@ def Get_input():
             Get_input()
     elif command == "item 1":
         if Combat == True:
-            pass
+            if player["item 1"]["Subtype"] == "Potion":
+                Use_Potion("item 1",player)
         else:
             print("there are no enemies nearby")
             Get_input()
     elif command == "item 2":
         if Combat == True:
-            pass
+            if player["item 2"]["Subtype"] == "Potion":
+                Use_Potion("item 2",player)
         else:
             print("there are no enemies nearby")
             Get_input()
     elif command == "save":
-        pass
-    elif command == "debug":
+        save()
+        Get_input()
+    elif command == "load":
+        load()
+        Get_input()
+    elif command == "stats":
         print(player)
         print(enemy_list)
         Get_input()
     else:
         print("invalid command")
         Get_input()
-#
+#saving/loading
+
+def save():
+    file = open("saves.txt","w")
+    file.write(str(player))
+    file.close()
+
+def Fix_Data(data):
+    while data.find("<") != -1:
+        index = data.find("<")
+        index_end = data.find(">")
+        startFI = data.find(" ",index)
+        endFI = data.find(" ", startFI + 1)
+        function = data[startFI:endFI]
+        data = data[:index] + function + data[index_end+1:]
+    return data
+
+def load():
+    file = open("saves.txt","r")
+    global player
+    data = file.read()
+    data = Fix_Data(data)
+    player = eval(data)
+
 
 # debug
 
 
 debug_weapon0 = {
     "Name" : "Worldsmasher",
+    "Rarity": 5,
+    "Modifier": none,
     "VitMod" : 10.0,
     "EndMod" : 20.0,
     "IntMod" : 5.0,
     "MinVit" : 0.0,
     "MinEnd" : 0.0,
     "MinInt" : 0.0,
+    "Weight" : "Heavy",
     "Type"  : "Weapon",
     }
 debug_weapon1 = {
     "Name" : "Solar Staff",
+    "Rarity": 5,
+    "Modifier": none,
     "VitMod" : 0.0,
     "EndMod" : 0.0,
     "IntMod" : 75.0,
     "MinVit" : 0.0,
     "MinEnd" : 0.0,
     "MinInt" : 400.0,
+    "Weight" : "Light",
+    "Type"  : "Weapon",
+    }
+debug_weapon2 = {
+    "Name" : "debug",
+    "Rarity": 5,
+    "Modifier": none,
+    "VitMod" : 0.0,
+    "EndMod" : 100000000.0,
+    "IntMod" : 0.0,
+    "MinVit" : 0.0,
+    "MinEnd" : 0.0,
+    "MinInt" : 400.0,
+    "Weight" : "Light",
     "Type"  : "Weapon",
     }
 debug_attack0 = {
@@ -334,11 +569,17 @@ enemy_list = []
 player = new_player(1,1)
 Lost = False
 Combat = False
+
+player["Weapon"] = Generate_Weapon(Get_Loot_Level(player["level"]))
+player["Armour"] = Generate_Armour(Get_Loot_Level(player["level"]))
+player["Ring"] = Generate_Ring()
 # debug init
-player["Weapon"] = debug_weapon0
+player["inv"].append(debug_weapon0)
 player["inv"].append(debug_weapon1)
+player["inv"].append(debug_weapon2)
 player["skills"].append(debug_attack0)
-print(player)
+
+
 
 # Game loop
 while Lost == False:
